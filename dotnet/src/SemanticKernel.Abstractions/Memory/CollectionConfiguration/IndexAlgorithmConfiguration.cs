4d9d2fa60deecd9074c8cbd2132a88496d71ac86
westey <164392973+westey-m@users.noreply.github.com>
2024-05-23 18:16:59 +0100

    Add idea for vector configuration when creating a collection.

---
+++ dotnet/src/SemanticKernel.Abstractions/Memory/CollectionConfiguration/IndexAlgorithmConfiguration.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.SemanticKernel.Memory.CollectionConfiguration;
+
+/// <summary>
+/// Configuration of an index algorithm to use when indexing a vector field.
+/// </summary>
+public class IndexAlgorithmConfiguration
+{
+    /// <summary>
+    /// The kind of index to use.
+    /// </summary>
+    public IndexKind IndexKind { get; set; }
+
+    /// <summary>
+    /// The distance function to use when comparing vectors.
+    /// </summary>
+    public DistanceFunction DistanceFunction { get; set; }
+}
---
+++ dotnet/src/SemanticKernel.Abstractions/Memory/CollectionConfiguration/VectorFieldConfiguration.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.SemanticKernel.Memory.CollectionConfiguration;
+
+/// <summary>
+/// Contains configuration for creating a vector field in a data store when creating a collection.
+/// </summary>
+public class VectorFieldConfiguration
+{
+    /// <summary>
+    /// The name of the field.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The data type that the vector will store.
+    /// </summary>
+    public VectorValueType VectorValueType { get; set; }
+
+    /// <summary>
+    /// The number of dimensions in the vector.
+    /// </summary>
+    public int VectorDimensions { get; set; }
+
+    /// <summary>
+    /// The configuration for the index algorithm to use when indexing the vector field.
+    /// </summary>
+    public IndexAlgorithmConfiguration IndexAlgorithmConfiguration { get; set; }
+}
