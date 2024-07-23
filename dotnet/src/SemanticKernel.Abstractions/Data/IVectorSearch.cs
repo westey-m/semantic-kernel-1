2db073694188329278b1ac8dc372212d255b10ad
westey <164392973+westey-m@users.noreply.github.com>
2024-07-23 17:07:46 +0100

    Add VectorSearch Abstraction.

---
+++ dotnet/src/SemanticKernel.Abstractions/Data/IVectorSearch.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+
+namespace Microsoft.SemanticKernel.Data;
+
+/// <summary>
+/// Interface for searching a vector store.
+/// </summary>
+/// <typeparam name="TRecord">The record data model to use for retrieving data from the store.</typeparam>
+[Experimental("SKEXP0001")]
+public interface IVectorSearch<TRecord>
+    where TRecord : class
+{
+    /// <summary>
+    /// Search the vector store for records that match the given embedding and filter.
+    /// </summary>
+    /// <param name="vectorQuery">The vector to search with.</param>
+    /// <param name="options">Optional options to configure the search.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>The records found by the vector search, including their result scores.</returns>
+    IAsyncEnumerable<VectorSearchResult<TRecord>> SearchAsync<TVectorElement>(VectorSearchQuery<TVectorElement> vectorQuery, VectorSearchOptions? options = default, CancellationToken cancellationToken = default);
+}
---
+++ dotnet/src/SemanticKernel.Abstractions/Data/VectorSearchTextEmbeddingQuery.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.SemanticKernel.Data;
+
+/// <summary>
+/// Contains text and the embedding generated from the text to use when searching a vector store.
+/// </summary>
+/// <typeparam name="TVectorElement">The type of the vector elements.</typeparam>
+[Experimental("SKEXP0001")]
+public sealed class VectorSearchTextEmbeddingQuery<TVectorElement> : VectorSearchQuery<TVectorElement>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="VectorSearchTextEmbeddingQuery{TVectorElement}"/> class.
+    /// </summary>
+    /// <param name="queryText">The text from which the embedding was generated.</param>
+    /// <param name="vector">The vector to use when searching the vector store.</param>
+    public VectorSearchTextEmbeddingQuery(string queryText, ReadOnlyMemory<TVectorElement> vector)
+    {
+        this.QueryText = queryText;
+        this.Vector = vector;
+    }
+
+    /// <summary>
+    /// The text from which the embedding was generated.
+    /// </summary>
+    public string QueryText { get; init; }
+}
